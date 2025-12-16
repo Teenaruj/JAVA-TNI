7d6fd371020ce020989f8718d18b96de484fe8c3
Teenaruj <106090783+Teenaruj@users.noreply.github.com>
2025-12-16 12:38:23 +0700

    ฑีนรุจิ์ ชูชมชื่น LabSheet05

---
+++ Lab506.java
@@ -0,0 +1,41 @@
+import javax.swing.JOptionPane;
+
+public class Lab506 {
+
+    public static boolean is_prime_number(int number) {
+        if (number <= 1) return false;
+
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        int number;
+
+        while (true) {
+            number = Integer.parseInt(
+                    JOptionPane.showInputDialog("Enter an integer:")
+            );
+
+            if (number > 1) {
+                break;
+            }
+        }
+
+        if (is_prime_number(number)) {
+            JOptionPane.showMessageDialog(
+                    null,
+                    number + " is Prime number"
+            );
+        } else {
+            JOptionPane.showMessageDialog(
+                    null,
+                    number + " is NOT Prime number"
+            );
+        }
+    }
+}
